3a87e5d814cad4fcfcbfb70c1169c2d7d086b4da
Lionel <dr.zuritalionel@gmail.com>
2025-07-18 01:08:52 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,18 @@
+
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.3.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ_OrIkI0whutfHXZRAZEl4IyOaX_8DXE",
+  authDomain: "estudiozuritaagenda.firebaseapp.com",
+  projectId: "estudiozuritaagenda",
+  storageBucket: "estudiozuritaagenda.firebasestorage.app",
+  messagingSenderId: "55134660545",
+  appId: "1:55134660545:web:107ce7f27588e32f8a7140"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
